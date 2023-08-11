7aa764b78ae21662f5c8aaaafbadaeec175a28bb
Aliaksei Bialenka <aliaksei.bialenka@innowise-group.com>
2023-08-11 16:52:43 +0200

    entity,repo,types,passwords

---
+++ apps/account/src/app/user/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { IUser, UserRole } from '@ms-nest-js/interfaces';
+import { compare, genSalt, hash } from 'bcryptjs';
+export class UserEntity implements IUser {
+  constructor(user: IUser) {
+    this._id = user._id;
+    this.displayName = user.displayName;
+    this.email = user.email;
+    this.role = user.role;
+  }
+  _id?: string;
+  displayName?: string;
+  email: string;
+  passwordHash: string;
+  role: UserRole;
+
+  public async setPassword(password: string) {
+    const salt = await genSalt(10);
+    this.passwordHash = await hash(password, salt);
+    return this;
+  }
+
+  public validatePassword(password: string) {
+    return compare(password, this.passwordHash);
+  }
+}
---
+++ apps/account/src/app/user/models/user.model.ts
@@ -0,0 +1,23 @@
+import { Document } from 'mongoose';
+import { IUser, UserRole } from '@ms-nest-js/interfaces';
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
+export class User extends Document implements IUser {
+  @Prop()
+  displayName?: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  passwordHash: string;
+
+  @Prop({
+    required: true,
+    enum: UserRole,
+    type: String,
+    default: UserRole.Student,
+  })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
